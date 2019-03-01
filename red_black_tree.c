93d35d97e49b0f89a3f7d7551b1e842931767306
lucas-montenegro <43447533+lucas-montenegro@users.noreply.github.com>
2019-03-01 17:19:39 -0300

    Update red_black_tree.c

--- red_black_tree.c
+++ red_black_tree.c
@@ -20,26 +20,50 @@ rbt *create_empty_red_black_tree() {
 	return new_rbt; 
 }
 
+bool update_color(rbt *rbt) {
+	if(rbt != NULL) {
+		if(rbt -> color == false) { // change from red to black
+			return true;
+		}
+
+		return false; // change from black to red
+	}
+
+	return true; // cant change if the node is NULL
+}
+
 rbt *left_rotation(rbt *r_b_t) {
+	rbt *rbt_right_child = NULL;
+
 	if(r_b_t != NULL && r_b_t -> right_child != NULL) {
-		rbt *rbt_right_child = r_b_t -> right_child;
-		r_b_t -> right_child = r_b_t -> right_child -> left_child;
+		rbt_right_child = r_b_t -> right_child;
+		r_b_t -> right_child = rbt_right_child -> left_child;
 		rbt_right_child -> left_child = r_b_t;
 	}
 
 	// code to update the color
+	rbt_right_child -> color = update_color(rbt_right_child);
+	rbt_right_child -> right_child -> color = update_color(rbt_right_child -> right_child);
+	r_b_t -> color = update_color(r_b_t);
+	r_b_t -> left_child -> color = update_color(r_b_t -> left_child);
 
 	return rbt_right_child;
 }
 
 rbt *right_rotation(rbt *r_b_t) {
+	rbt *rbt_left_child = NULL;
+
 	if(r_b_t != NULL && r_b_t -> left_child != NULL) {
-		rbt *rbt_left_child = r_b_t -> left_child;
-		r_b_t -> left_child = r_b_t -> left_child -> right_child;
+		rbt_left_child = r_b_t -> left_child;
+		r_b_t -> left_child = rbt_left_child -> right_child;
 		rbt_left_child -> right_child = r_b_t;
 	}
 
 	// code to update the color
+	rbt_left_child -> color = update_color(rbt_left_child);
+	rbt_left_child -> left_child -> color = update_color(rbt_left_child -> left_child);
+	r_b_t -> color = update_color(r_b_t);
+	r_b_t -> right_child -> color = update_color(r_b_t -> right_child);
 
 	return rbt_left_child;
 }
@@ -53,8 +77,4 @@ int main() {
 
 /* references:
 https://en.wikipedia.org/wiki/Red–black_treehttps://en.wikipedia.org/wiki/Red–black_tree
-
-
-
-
 */
