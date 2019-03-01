790e291fd8162743225869933dfe7db806b9ecfc
lucas-montenegro <43447533+lucas-montenegro@users.noreply.github.com>
2019-03-01 16:29:38 -0300

    Update red_black_tree.c

--- red_black_tree.c
+++ red_black_tree.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct red_black_tree rbt;
+
+struct red_black_tree { 
+	bool color; // defines the color red or black 
+	int item;
+	rbt *right_child;
+	rbt *left_child;
+};
+
 
 int main() {
-  return 0;
+
+
+	return 0;
 }
+
+/* references:
+https://en.wikipedia.org/wiki/Red–black_treehttps://en.wikipedia.org/wiki/Red–black_tree
+
+
+
+
+*/
