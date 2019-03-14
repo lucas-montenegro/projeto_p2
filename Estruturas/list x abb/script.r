7f062f7b7370def2236a516784bf115d527f70e6
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-03-13 21:59:39 -0300

    ulti reaj.

--- Estruturas/list x abb/script.r
+++ Estruturas/list x abb/script.r
@@ -1,7 +1,13 @@
 bt = read.csv(file = "saida_bt.csv")
 list = read.csv(file = "saida_list.csv")
 
+comparison_bt = bt$comparison
+size_bt = bt$size
+
+comparison_list = list$comparison
+size_list = list$size
+
 par(las=1, cex = 1, bty = "l", tcl = -0.2, col="black", family="serif", mfrow = c(2,1), oma =  c(4,1,1,1))
 
-plot(bt, xlab = "Tamanho", ylab = "Quantidade de comparações")  
-plot(list, xlab = "Tamanho", ylab = "Quantidade de comparações")
+plot(comparison_bt~size_bt, xlab = "Tamanho", ylab = "Quantidade de comparações")  
+plot(comparison_list~size_list, xlab = "Tamanho", ylab = "Quantidade de comparações")  
