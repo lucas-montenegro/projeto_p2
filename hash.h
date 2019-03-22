5751ba3282454f86ae0cf46dc4f45018d34d6ba0
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-03-22 18:15:31 -0300

    Alterations

--- hash.h
+++
@@ -1,9 +0,0 @@
-typedef struct hash{
-    huff *table[256];
-}hash;
-
-extern hash *create_hash();
-
-extern void put_hash(hash *hash, unsigned char *read_byte);
-
-extern hash *read_archive(char *name_file);
