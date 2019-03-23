85e5918f63da87b2279b76d2d929fbcaa73207ad
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-03-23 10:29:54 -0300

    update

--- Huffman/descompres.c
+++ Huffman/descompres.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct t_b binary_t;
 
 struct t_b 
@@ -9,9 +8,9 @@ struct t_b
 	unsigned char item;
 	binary_t *left;
 	binary_t *right;
-}
+};
 
-binary_t *create_binary_tree(unsigned char byte, binary_t *left, binary_t*right)
+binary_t *create_binary_tree(unsigned char byte, binary_t *left, binary_t* right)
 {
 	binary_t *new_node = (binary_t *) malloc(sizeof(binary_t));
 	new_node->item = byte;
@@ -21,51 +20,96 @@ binary_t *create_binary_tree(unsigned char byte, binary_t *left, binary_t*right)
 	return new_node;
 }
 
-
-binary_t *create_binary_tree(binary_t* bt, FILE *archive, short size, short count)
+void bt_print (binary_t *bt)
 {
-	
+	if(bt == NULL)
+		return;
+
+	printf("%x ", bt->item);
+	bt_print(bt->left);
+	bt_print(bt->right);
 }
+
+void create_t_reading(binary_t **bt, FILE *archive, short size, short *count)
+{
+	if (size == *count)
+		return;
+
+	unsigned char byte;
+	*count += 1;
+
+	if (fscanf(archive,	"%c", &byte) == EOF)
+	{
+		printf("Archive incomplete\n");
+
+		return;
+	}
+
+	if (byte == '*')
+	{
+		*bt = create_binary_tree(byte, NULL, NULL);
+		create_t_reading(&((*bt)->left), archive, size, count);
+		create_t_reading(&((*bt)->right), archive, size, count);
+	}
+	else if (byte == (char)(92))
+	{
+		*count += 1;
+
+		if (fscanf(archive,	"%c", &byte) == EOF)
+		{
+			printf("Archive incomplete\n");
+
+			return;
+		}
+
+		*bt = create_binary_tree(byte, NULL, NULL);
+	}
+	else
+	{
+		*bt = create_binary_tree(byte, NULL, NULL);	
+	}
+}
+
 
 void decompactar(char *name_file) 
 {
 	short *count;
 	unsigned short trash, size_tree;
-	unsigned char *byte = (unsigned char*) malloc(sizeof(unsigned char)); 
-	unsigned char *byte_aux = (unsigned char*) malloc(sizeof(unsigned char)); 
-	FILE *archive = fread(name_file, "wb");
-	binary_t *b_tree;
+	unsigned char byte, byte_aux;
+	FILE *archive = fopen(name_file, "rb");
+	binary_t *b_tree = NULL;
 
-	if (fscanf(archive,	"%c", byte) == EOF)
+	if (fscanf(archive,	"%c", &byte) == EOF)
 	{
 		printf("Archive incomplete\n");
 
 		return;
 	}
 
-	*byte_aux = *byte;
-	*byte_aux >> 5;
-	trash = (short) *byte_aux;
+	byte_aux = byte;
+	byte_aux >> 5;
+	trash = (short) byte_aux;
 
 
-	*byte_aux = *byte;
-	(*byte_aux) << 3;
-	(*byte_aux) >> 3;
-	size_tree = *byte_aux;
+	byte_aux = byte;
+	byte_aux << 3;
+	byte_aux >> 3;
+	size_tree = byte_aux;
 	size_tree << 8;
 
-	if (fscanf(archive,	"%c", byte) == EOF)
+	if (fscanf(archive,	"%c", &byte) == EOF)
 	{
 		printf("Archive incomplete\n");
 
 		return;
 	}
 
-	size_tree = size_tree | ((unsigned short) (*byte));
+	size_tree = size_tree | ((unsigned short) byte);
 
-
-	create_binary_tree(NULLNULL, NULL);
 
 	*count = 0;
-	create_t_reading (archive, size_tree, count);
+	create_t_reading (&b_tree, archive, size_tree, count);
+
+	bt_print(b_tree);
+	printf("\n");
 }
