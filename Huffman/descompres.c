a6d630f102c078daafec7103100cafee792c5be2
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-03-23 13:30:42 -0300

    eoq

--- Huffman/descompres.c
+++ Huffman/descompres.c
@@ -30,17 +30,17 @@ void bt_print (binary_t *bt)
 	bt_print(bt->right);
 }
 
-void create_t_reading(binary_t **bt, FILE *archive, short size, short *count)
+void create_t_reading(binary_t **bt, FILE *file, short size, short *count)
 {
-	if (size == *count)
+	if (*count == size)
 		return;
 
 	unsigned char byte;
 	*count += 1;
 
-	if (fscanf(archive,	"%c", &byte) == EOF)
+	if (fscanf(file, "%c", &byte) == EOF)
 	{
-		printf("Archive incomplete\n");
+		printf("file incomplete\n");
 
 		return;
 	}
@@ -48,16 +48,16 @@ void create_t_reading(binary_t **bt, FILE *archive, short size, short *count)
 	if (byte == '*')
 	{
 		*bt = create_binary_tree(byte, NULL, NULL);
-		create_t_reading(&((*bt)->left), archive, size, count);
-		create_t_reading(&((*bt)->right), archive, size, count);
+		create_t_reading(&((*bt)->left), file, size, count);
+		create_t_reading(&((*bt)->right), file, size, count);
 	}
-	else if (byte == (char)(92))
+	else if (byte == '\\')
 	{
 		*count += 1;
 
-		if (fscanf(archive,	"%c", &byte) == EOF)
+		if (fscanf(file, "%c", &byte) == EOF)
 		{
-			printf("Archive incomplete\n");
+			printf("file incomplete\n");
 
 			return;
 		}
@@ -71,17 +71,16 @@ void create_t_reading(binary_t **bt, FILE *archive, short size, short *count)
 }
 
 
-void decompactar(char *name_file) 
+void descompress(char *name_file) 
 {
-	short *count;
-	unsigned short trash, size_tree;
+	unsigned short trash, size_tree; //Tamanho das partes dos arquivos
 	unsigned char byte, byte_aux;
-	FILE *archive = fopen(name_file, "rb");
+	FILE *file = fopen(name_file, "rb");
 	binary_t *b_tree = NULL;
 
-	if (fscanf(archive,	"%c", &byte) == EOF)
+	if (fscanf(file, "%c", &byte) == EOF)
 	{
-		printf("Archive incomplete\n");
+		printf("file incomplete\n");
 
 		return;
 	}
@@ -90,25 +89,23 @@ void decompactar(char *name_file)
 	byte_aux >> 5;
 	trash = (short) byte_aux;
 
-
 	byte_aux = byte;
 	byte_aux << 3;
 	byte_aux >> 3;
-	size_tree = byte_aux;
+	size_tree = (short) byte_aux;
 	size_tree << 8;
 
-	if (fscanf(archive,	"%c", &byte) == EOF)
+	if (fscanf(file, "%c", &byte) == EOF)
 	{
-		printf("Archive incomplete\n");
+		printf("file incomplete\n");
 
 		return;
 	}
 
 	size_tree = size_tree | ((unsigned short) byte);
+	short count = 0;
 
-
-	*count = 0;
-	create_t_reading (&b_tree, archive, size_tree, count);
+	create_t_reading(&b_tree, file, size_tree, &count);
 
 	bt_print(b_tree);
 	printf("\n");
