ce3b35353a32ec4182a28180538ae0f0d1675cce
Lucas Montenegro Andrade Assunção <43447533+lucas-montenegro@users.noreply.github.com>
2019-03-23 22:24:27 -0300

    Update descompress.h

--- Huffman/descompress.h
+++ Huffman/descompress.h
@@ -1,3 +1,11 @@
+typedef struct b_t binary_t;
+
 extern void create_t_reading(binary_t **bt, FILE *file, short size, short *count);
 
 extern void descompress(char *name_file);
+
+extern void treat_string_2(char **name_file);
+
+extern void read_descompress(FILE *file, FILE *new_file, binary_t *b_tree, unsigned short trash);
+
+extern unsigned char read_bit(unsigned char byte, int cont);
